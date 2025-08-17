a1d5440677ec6d0c69710ec8cfa4e627373f2266
つづこ <>
2025-08-17 09:54:28 +0900

    routeの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
-  resources :memos
+
+  root "memos#index"
+
+  resources :memos do
+    member do
+      patch :toggle # ② チェックで完了/未完了トグル
+    end
+     collection do
+       get :completed # ③ 完了済み一覧に分離
+     end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
   # root "posts#index"
